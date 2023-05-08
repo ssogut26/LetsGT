2d1d6a6d8c84cd2d554449186fc5280d23082d95
Selcuk <ssogut26@gmail.com>
2023-05-08 14:08:19 +0300

    actions test

--- lib/config/routes/routes.dart
+++ lib/config/routes/routes.dart
@@ -12,7 +12,7 @@ part 'routes.gr.dart';
 class AppRouter extends _$AppRouter {
   @override
   List<AutoRoute> get routes => [
-        AutoRoute(page: HomeRoute.page),
+        AutoRoute(page: HomeRoute.page, initial: true),
         AutoRoute(page: SignInRoute.page),
         AutoRoute(page: SignUpRoute.page),
       ];
